b6a3ea8409be83b472baed67863299777eb8053c
vaibhav Bansode <98651009+vaibhav702@users.noreply.github.com>
2022-04-08 14:01:03 +0530

    Update urlController.js

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,16 +1,15 @@
 const urlModel = require("../model/urlModel");
-const shortId = require("shortid")
-const validator = require("../validator/validator")
-const validUrl = require('valid-url');
+const shortId = require("shortid");
+const validator = require("../validator/validator");
+const validUrl = require("valid-url");
 const redis = require("redis");
 const { promisify } = require("util");
 
-
 //Connect to redis (Remote Dictionary Server)
 const redisClient = redis.createClient(
-  11588,  // port number
-  "redis-11588.c212.ap-south-1-1.ec2.cloud.redislabs.com",  // link sring( Link)
-  { no_ready_check: true }  // 
+  11588, // port number
+  "redis-11588.c212.ap-south-1-1.ec2.cloud.redislabs.com", // link sring( Link)
+  { no_ready_check: true } //
 );
 redisClient.auth("6W26UpQbpdlmIq12YAQU8vSxeWVkc6cS", function (err) {
   if (err) throw err;
@@ -19,7 +18,6 @@ redisClient.auth("6W26UpQbpdlmIq12YAQU8vSxeWVkc6cS", function (err) {
 redisClient.on("connect", async function () {
   console.log("Connected to Redis..");
 });
-
 
 // Redis credentials
 // Link : redis-16368.c15.us-east-1-2.ec2.cloud.redislabs.com
@@ -32,60 +30,68 @@ redisClient.on("connect", async function () {
 
 const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
-const SET_EXP = promisify(redisClient.expire).bind(redisClient)
-
-
-
-
-
-
+const SET_EXP = promisify(redisClient.expire).bind(redisClient);
 
 const createUrl = async (req, res) => {
-
   try {
-    const keysArray = Object.keys(req.body)
-    console.log(keysArray)
+    const keysArray = Object.keys(req.body);
+    console.log(keysArray);
     if (!keysArray.length) {
-      return res.status(400).send({ status: false, message: "request body is empty please provide longUrl" })
+      return res
+        .status(400)
+        .send({
+          status: false,
+          message: "request body is empty please provide longUrl",
+        });
     }
     const longUrl = req.body.longUrl;
 
-
     if (!validator.isValid(longUrl)) {
-      return res.status(400).send({ status: false, message: "longUrl is not present please provide longUrl" })
+      return res
+        .status(400)
+        .send({
+          status: false,
+          message: "longUrl is not present please provide longUrl",
+        });
     }
     if (!(keysArray.length === 1 && keysArray.includes("longUrl"))) {
-      return res.status(400).send({ status: false, message: "Error!: Invalid input Only longUrl is accepted" })
+      return res
+        .status(400)
+        .send({
+          status: false,
+          message: "Error!: Invalid input Only longUrl is accepted",
+        });
     }
     if (!validUrl.isUri(longUrl)) {
-      return res.status(400).send({ status: false, message: "invalid url please provide valid url" })
+      return res
+        .status(400)
+        .send({
+          status: false,
+          message: "invalid url please provide valid url",
+        });
     }
-    console.log(longUrl)
-    const base = "http://localhost:3000";//our server base code
+    console.log(longUrl);
+    const base = "http://localhost:3000"; //our server base code
     let urlCode = shortId.generate().toLowerCase();
 
+    console.log(urlCode);
 
-    console.log(urlCode)
-
-    let url = await urlModel.findOne({ longUrl }).select({id:0,_v:0});
+    let url = await urlModel.findOne({ longUrl }).select({ id: 0, _v: 0 });
     //It describes the internal revision of a document. This __v field is used to track the revisions of a document. By default, its value is zero.
     if (url) {
-      return res.status(200).send({status:true,data:url});
+      return res.status(200).send({ status: true, data: url });
     } else {
-
-      let shortUrl = `${base}/${urlCode}`
+      let shortUrl = `${base}/${urlCode}`;
       // const shortUrl = "http://localhost:3000/nhnmecrau";;
       const generateUrl = () => {
         urlCode = shortId.generate().toLowerCase();
         return `${base}/${urlCode}`;
+      };
 
-      }
-
-
-      const duplicateUrl = await urlModel.findOne({ shortUrl })
+      const duplicateUrl = await urlModel.findOne({ shortUrl });
 
       if (duplicateUrl) {
-        shortUrl = generateUrl()
+        shortUrl = generateUrl();
         // return res.send({status: 409, message:"please again hit this API to generate shortUrl"})
       }
 
@@ -93,53 +99,53 @@ const createUrl = async (req, res) => {
         longUrl,
         shortUrl,
         urlCode,
-        
       };
       //if url short is created already so we are going to find it and give that url to user
-      const dbUrl = await urlModel.create(newUrl).select({_id:0});
+      const dbUrl = await urlModel.create(newUrl).select({ _id: 0 });
       return res.status(201).send({ status: true, data: dbUrl });
-
     }
-
   } catch (error) {
-    return res.status(500).send({ status: false, message: error.message })
+    return res.status(500).send({ status: false, message: error.message });
   }
-}
-
+};
 
 const getUrl = async (req, res) => {
-
   try {
     const urlCode = req.params.urlCode;
 
     if (!validator.isValid(urlCode)) {
-      return res.status(400).send({ status: false, message: "longUrl is not present please provide longUrl" })
+      return res
+        .status(400)
+        .send({
+          status: false,
+          message: "longUrl is not present please provide longUrl",
+        });
     }
 
-
-    const redis_string_UrlDoc = await GET_ASYNC(`${req.params.urlCode}`)
-    console.log(redis_string_UrlDoc)
-    const redis_UrlDoc = JSON.parse(redis_string_UrlDoc)
+    const redis_string_UrlDoc = await GET_ASYNC(`${req.params.urlCode}`);
+    console.log(redis_string_UrlDoc);
+    const redis_UrlDoc = JSON.parse(redis_string_UrlDoc);
     if (redis_UrlDoc) {
-      console.log("responce from redis")
-      return res.status(303).redirect(redis_UrlDoc.longUrl)
+      console.log("responce from redis");
+      return res.status(303).redirect(redis_UrlDoc.longUrl);
     }
 
-    const urlDocument = await urlModel.findOne({ urlCode: urlCode }).select({ _id: 0, urlCode: 1, longUrl: 1 })
+    const urlDocument = await urlModel
+      .findOne({ urlCode: urlCode })
+      .select({ _id: 0, urlCode: 1, longUrl: 1 });
     if (urlDocument == null) {
-      return res.status(404).send({ status: false, msg: "url Document not Found" })
+      return res
+        .status(404)
+        .send({ status: false, msg: "url Document not Found" });
     }
-    await SET_ASYNC(`${urlCode}`, JSON.stringify(urlDocument))
-    await SET_EXP(`${urlCode}`, 60)
-    const longUrl = urlDocument.longUrl
-    return res.status(302).redirect(longUrl)
-
+    await SET_ASYNC(`${urlCode}`, JSON.stringify(urlDocument));
+    await SET_EXP(`${urlCode}`, 60);
+    const longUrl = urlDocument.longUrl;
+    return res.status(302).redirect(longUrl);
   } catch (error) {
-    return res.status(500).send({ status: false, message: error.message })
+    return res.status(500).send({ status: false, message: error.message });
   }
-
-}
-
+};
 
 module.exports.createUrl = createUrl;
 module.exports.getUrl = getUrl;
