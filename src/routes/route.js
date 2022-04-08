924f50cb7756bc0d021aad74a551ebb3864a45c1
vaibhav Bansode <98651009+vaibhav702@users.noreply.github.com>
2022-04-08 14:03:02 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,14 @@
-
 const express = require("express");
 const router = express.Router();
 const urlController =require("../controller/urlController");
 
 //------------------urlCOntroller--------------------------------------------//
+//create shortUrl
 router.post("/url/shorten", urlController.createUrl);
+//get shortUrl
 router.get("/:urlCode", urlController.getUrl);
 
+//page not found
 router.get("*", function (req,res){
     return res.status(404).send("ERROR!: page not found")
 });
